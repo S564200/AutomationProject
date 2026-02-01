291c3893ffcf1bbc21228ef46a6506af8a660c1b
PARTHA SARATHY BODA <S564200@nwmissouri.edu>
2026-02-01 16:23:45 -0600

    Logger
    Added Logger

--- src/test/java/com/orangehrm/app/pageobjects/Myinfo.java
+++ src/test/java/com/orangehrm/app/pageobjects/Myinfo.java
@@ -2,6 +2,8 @@ package com.orangehrm.app.pageobjects;
 
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,10 +14,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.orangehrm.app.testCases.Baseclass;
+
 public class Myinfo {
 	
 	WebDriver ldriver;
 	WebDriverWait wait;
+	private static final Logger logger = LogManager.getLogger(Myinfo.class);
 	public Myinfo(WebDriver rdriver)
 	{
 	ldriver = rdriver;
@@ -29,7 +34,7 @@ public class Myinfo {
 	@FindBy(xpath = "//button[normalize-space()='Save']") WebElement saveButton;
 	@FindBy(xpath = "//input[@placeholder=\"Type for hints...\"]") WebElement searchemp;
 	@FindBy(xpath = "//div[contains(@class,'oxd-select-text')]") WebElement dropdown;
-	@FindBy(xpath = "//span[text()='Automation Tester']") WebElement selectcourse;
+	@FindBy(xpath = "//span[text()='Automaton Tester']") WebElement selectcourse;
 	@FindBy(xpath = "//button[@type=\"submit\"]") WebElement searchbutton;
 	@FindBy(xpath = "//a[@href=\"/web/index.php/time/viewTimeModule\"]") WebElement Timeselect;
 	@FindBy(xpath = "//input[@placeholder=\"Type for hints...\"]") WebElement selectEmployee;
@@ -49,79 +54,99 @@ public class Myinfo {
 	@FindBy(xpath = "//label[contains(text(),'License Number')]/ancestor::div[contains(@class,'oxd-input-group')]//input") WebElement DLField;
 	
 	public void PIMDemo() throws InterruptedException {
-		Thread.sleep(3000);
-		wait.until(ExpectedConditions.elementToBeClickable(PIMButton)).click();
-		wait.until(ExpectedConditions.visibilityOf(emplyeename)).sendKeys("exampletest");
-		Thread.sleep(3000);
-		wait.until(ExpectedConditions.visibilityOf(empid)).sendKeys("54678");
-		wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
-		Thread.sleep(2000);
-		wait.until(ExpectedConditions.elementToBeClickable(empid)).click();
 
-			    // for clearing the searched entry
-			    Actions pm = new Actions(ldriver);
-			    pm.keyDown(Keys.CONTROL)
-			     .sendKeys("a")
-			      .keyUp(Keys.CONTROL)
-			      .sendKeys(Keys.BACK_SPACE)
-			      .build()
-			      .perform();
-			}
+	    logger.info("Navigating to PIM module");
+	    wait.until(ExpectedConditions.elementToBeClickable(PIMButton)).click();
+	    Thread.sleep(3000);
+	    logger.info("Entering employee name: exampletest");
+	    wait.until(ExpectedConditions.visibilityOf(emplyeename)).sendKeys("exampletest");
+	    Thread.sleep(2000);
+	    logger.info("Entering employee ID: 54678");
+	    wait.until(ExpectedConditions.visibilityOf(empid)).sendKeys("54678");
+	    Thread.sleep(3000);
+	    logger.info("Clicking Search button");
+	    wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
+	    Thread.sleep(2000);
+	    logger.info("Clearing employee ID field after search");
+	    wait.until(ExpectedConditions.elementToBeClickable(empid)).click();
+
+	    Actions pm = new Actions(ldriver);
+	    pm.keyDown(Keys.CONTROL)
+	      .sendKeys("a")
+	      .keyUp(Keys.CONTROL)
+	      .sendKeys(Keys.BACK_SPACE)
+	      .perform();
+
+	    logger.info("Employee ID field cleared successfully");
+	}
+
 	
 	
 	
 	//this method is for Myinfo page
 	public void savebutton() throws InterruptedException {
 		Thread.sleep(3000);
+		logger.info("Navigating to My Info page");
 		wait.until(ExpectedConditions.elementToBeClickable(MyinfoInput)).click();
 		Thread.sleep(3000);
 		// created actions class for clearning the existing data and for scrolling to particular element.
+		 logger.info("Clearing middle name field");
 		Actions ac = new Actions(ldriver);
 		wait.until(ExpectedConditions.elementToBeClickable(inputname)).click();
-		
+		logger.info("Clearing middle name field");
 		ac.keyDown(Keys.CONTROL);
 		ac.sendKeys("a");
 		ac.keyUp(Keys.CONTROL);
 		ac.sendKeys(Keys.BACK_SPACE);
 		ac.build().perform();
-		inputname.sendKeys("testdemo5");
-		//Thread.sleep(2000);
+		logger.info("Updating middle name to: Jonah");
+		inputname.sendKeys("Jonah");
+		//Thread.sleep(3000);
 wait.until(ExpectedConditions.elementToBeClickable(DLField)).click();
 //		ac.keyDown(Keys.CONTROL);
 //		ac.sendKeys("a");
 //		ac.keyUp(Keys.CONTROL);
 //		ac.sendKeys(Keys.BACK_SPACE);
 //		ac.build().perform();
+logger.info("Clearing Driving License field");
        DLField.clear();
        Thread.sleep(3000);
-       DLField.sendKeys("DL-456234");
-
-		
-		//Thread.sleep(5000);
+       logger.info("Updating Driving License number");
+       DLField.sendKeys("DL-456234");		
+		Thread.sleep(5000);
 		ac.scrollToElement(saveButton).build().perform();
 		Thread.sleep(3000);
 		saveButton.click();
+		logger.info("My Info details saved successfully");
 		
 	}
 	
 	public void directoryinfo() throws InterruptedException {
 
 	    Actions actions = new Actions(ldriver);
+	    logger.info("Navigating to MyDirectory page");
 	    wait.until(ExpectedConditions.elementToBeClickable(MyDirectory)).click();
+	    logger.info("Searching with a random name");
 	    wait.until(ExpectedConditions.visibilityOf(searchemp)).sendKeys("Minh exampletest Nguyen");
 	  //  Thread.sleep(3000);
+	    logger.info("Clicked the dropdown");
 	    wait.until(ExpectedConditions.elementToBeClickable(dropdown));
 	    actions.moveToElement(dropdown).click().perform();
 	   Thread.sleep(3000);
+	   logger.info("Selected course");
 	    wait.until(ExpectedConditions.visibilityOf(selectcourse));
 	    actions.moveToElement(selectcourse).click().perform();
+	    logger.info("Search operation is done");
 	    wait.until(ExpectedConditions.elementToBeClickable(searchbutton)).click();
 	}
 	
 	public void Recruitmentpage() throws InterruptedException {
+		logger.info("Navigating to Recruitementpage");
 		wait.until(ExpectedConditions.elementToBeClickable(RecruitmentButton)).click();
+		logger.info("Clicked add button");
 		wait.until(ExpectedConditions.elementToBeClickable(addButton)).click();
 		Thread.sleep(3000);
+		logger.info("Added required fields");
 		wait.until(ExpectedConditions.visibilityOf(firstname)).sendKeys("exampletestfirst");
 		Thread.sleep(3000);
 		wait.until(ExpectedConditions.visibilityOf(lastname)).sendKeys("lastnametest");
@@ -132,6 +157,7 @@ wait.until(ExpectedConditions.elementToBeClickable(DLField)).click();
 		wait.until(ExpectedConditions.visibilityOf(email)).sendKeys("example@gmail.com");
 		Thread.sleep(3000);
 		Actions acr = new Actions(ldriver);
+		logger.info("Added details successfully");
 		acr.scrollToElement(submitButton);
 		acr.build().perform();
 		submitButton.click();
@@ -139,10 +165,13 @@ wait.until(ExpectedConditions.elementToBeClickable(DLField)).click();
 	}
 	
 	public void Timesheetmethod() throws InterruptedException {
+		logger.info("Navigating to Timesheet page Module");
 		 wait.until(ExpectedConditions.elementToBeClickable(Timeselect)).click();
 		Thread.sleep(3000);
+		logger.info("Searching a employee name");
 		selectEmployee.sendKeys("manda demotest user2");
 		Thread.sleep(3000);
+		logger.info("Clicked view Button");
 		viewButton.click();
 		
 		
--- src/test/java/com/orangehrm/app/testCases/Baseclass.java
+++ src/test/java/com/orangehrm/app/testCases/Baseclass.java
@@ -1,5 +1,7 @@
 package com.orangehrm.app.testCases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,6 +17,7 @@ public class Baseclass {
 
     public static WebDriver driver;
     public static ReadConfig readconfig;
+    private static final Logger logger = LogManager.getLogger(Baseclass.class);
 
     public static String ApplicationUrl;
     public static String userName;
@@ -23,27 +26,32 @@ public class Baseclass {
     @BeforeSuite
     @Parameters("browser")
     public void setup(@Optional("chrome") String br) throws InterruptedException {
+        logger.info("===== Test Suite Started successfully =====");
 
         readconfig = new ReadConfig();
 
         ApplicationUrl = readconfig.getFromConfingFile("ApplicationURL");
         userName = readconfig.getFromConfingFile("username");
         password = readconfig.getFromConfingFile("password");
+        logger.info("Browser selected: {}", br);
 
         if (br.equalsIgnoreCase("chrome")) {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--remote-allow-origins=*");
             driver = new ChromeDriver(options);
+            logger.info("Chrome browser launched successfully");
 
         } else if (br.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.gecko.driver",
                     readconfig.getfirefoxPath());
             driver = new FirefoxDriver();
+            logger.info("Firefox launched successfully");
         }
 
         driver.manage().window().maximize();
         driver.get(ApplicationUrl);
-        System.out.println("URL = " + ApplicationUrl);
+       // System.out.println("URL = " + ApplicationUrl);
+        logger.info("Navigated to URL: {}", ApplicationUrl);
 
         Thread.sleep(5000);
     }
@@ -52,7 +60,8 @@ public class Baseclass {
     public void tearDown() throws InterruptedException {
     	    Thread.sleep(3000);
             driver.quit();
-            System.out.println("after suite");
+            //System.out.println("after suite");
+            logger.info(" All the test case operations are done and Browser closed");
         }
     }
 
